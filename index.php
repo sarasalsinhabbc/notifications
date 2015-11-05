903bdc284d84d7eca4df3184da5a9521f79d035f
Vinny Singh <me@vinnysingh.co>
2015-11-05 10:31:24 +0000

    Made PHP version of index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!-- Load Header -->
+<?php include('header.php'); ?>
+
+	<!-- Load ORB -->
+	<?php include('orb.php'); ?>
+
+	<!-- Load Notifications / Tray -->
+	<?php inlcude('notifications.php');
+
+<!-- Load Footer -->
+<?php include('footer.php'); ?>
