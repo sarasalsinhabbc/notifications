5da196869175bac7fc92586aede6e27875086ebc
Vinny Singh <me@vinnysingh.co>
2015-11-05 12:38:07 +0000

    Added lounge header php

---
+++ notification_header_lounge.php
@@ -0,0 +1,9 @@
+<div id="loungeHeader">
+    <div class='wrapper'>
+        <p class="title">Notifications</p>
+        <div class="trayMenu">
+            <div class="button"><img src="imgs/flag_white.svg"><p class="longPrimer">Flagged</p></div>
+            <div class="button"><img src="imgs/settings_white.svg"><p class="longPrimer">Settings</p></div>
+        </div>
+    </div>
+</div>
