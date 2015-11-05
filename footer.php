4365383f12c7fe21512198dfca6e4c347920d36b
Vinny Singh <me@vinnysingh.co>
2015-11-05 10:30:36 +0000

    Added footer.php

---
+++ footer.php
@@ -0,0 +1,16 @@
+    <!--BBC Website-->
+    <div class="separator"></div>
+    <div class="separator"></div>
+
+    <div class="websiteContainer">
+        <div class="websiteContainer2">
+            <object class="website" type="text/html" data="http://www.bbc.co.uk/news"></object>
+        </div>
+    </div>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script src="custom.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    
+</body>
+</html>
