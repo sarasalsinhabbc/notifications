4fd481e9b672c3601e5cfb4d54c3d51463c3d9d0
Vinny Singh <me@vinnysingh.co>
2015-11-05 12:38:17 +0000

    updated notification header page

---
+++ notification_header_home.php
@@ -0,0 +1,21 @@
+<div class='wrapper'>
+    <div id="trayHeader">
+        <p class="title">NOTIFICATIONS</p>
+            <div class="trayMenu">
+                <a href="#" id="flag_button">
+                
+                    <div class="button">
+                        <img id="flag_icon" src="imgs/flag.svg" data-swap="imgs/flagged_new.svg">
+                        <p class="longPrimer">Flagged</p>
+                    </div>
+
+                </a>
+
+                <div class="button">
+                    <img src="imgs/settings.svg">
+                    <p class="longPrimer">Settings</p>
+                </div>
+
+            </div>
+    </div>
+</div>
